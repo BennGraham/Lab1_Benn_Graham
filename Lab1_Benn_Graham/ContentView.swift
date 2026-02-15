9f5094c84695cf215abf9e7c560c986e63f37970
BennGraham <benn.graham@georgebrown.ca>
2026-02-15 15:59:59 -0500

    Initial Commit

---
+++ Lab1_Benn_Graham/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lab1_Benn_Graham
+//
+//  Created by Benn Graham on 2026-02-15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lab1_Benn_Graham/Lab1_Benn_GrahamApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab1_Benn_GrahamApp.swift
+//  Lab1_Benn_Graham
+//
+//  Created by Benn Graham on 2026-02-15.
+//
+
+import SwiftUI
+
+@main
+struct Lab1_Benn_GrahamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
